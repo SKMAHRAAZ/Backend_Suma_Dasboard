426a3a2f1eb49d6a9debb8af85e4c49413e11d02
Saki9891 <saikirankollimalla@gmail.com>
2025-02-01 20:07:21 +0530

    don't be silly

--- src/VendorDasboard/Pages/RegisterPage.jsx
+++ src/VendorDasboard/Pages/RegisterPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Sidebar from '../Components/Sidebar'
 import RegisterForm from '../Components/forms/RegisterForm'
-import Register from '../Components/forms/register'
+
 
 
 const RegisterPage = () => {
